39f4187654aa7fd90fd8deddb572cf6bf989b3a6
陈哲宇 <1452100100@qq.com>
2019-01-22 20:21:42 +0800

    增加 构建时选择框架主题色

--- meta.js
+++ meta.js
@@ -10,15 +10,20 @@ module.exports = {
     },
 
   'prompts': {
-      "name": {
-        "type": "input",
-        "required": true,
-        "message": "项目名"
-      },
-      "author": {
-        "type": "input",
-        "message": "作者"
-      },
+    'name': {
+      'type': 'input',
+      'required': true,
+      'message': '项目名'
+    },
+    'author': {
+      'type': 'input',
+      'message': '作者'
+    },
+    'iviewColor': {
+      'type': 'input',
+      'message': 'iview 的主题色',
+      'default': '#ff7300'
+    }
   },
 
   filters: {//根据命令行交互的结果过滤将要渲染的项目文件
